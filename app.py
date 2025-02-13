3d6e39c6cb41b353979c746e3ed90103ad3d91f4
Sebas <162532976+Sebaxx-23@users.noreply.github.com>
2025-02-13 18:31:18 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Chatbot en Streamlit")
+st.titlte("Escribe tu mensaje y presiona 'Enviar'.")
+
+user_input = st.text_input("Tu mensaje:", "")
+
+if st.button("Enviar"):
+    if user_input:
+        st.write("Tu mensaje:", user_input)
+    else:
+        st.warning("Por favor, escribe un mensaje.")
